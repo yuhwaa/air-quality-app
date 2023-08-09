20b7d1f81115a023b7a6558c1e4f8a62b888362f
Yu <yuhwachn@gmail.com>
2023-08-08 23:32:11 -0400

    initial styling y

--- air-quality-app/src/AirQualityCard.js
+++ air-quality-app/src/AirQualityCard.js
@@ -3,11 +3,13 @@ import React from  'react'
 const AirQualityCard = ({data}) => {
   const { aqi, city, dominentpol, time } = data
   return (
-    <div>
-        <h5>{city.name}</h5>
-        <h6>Air Quality Index: {aqi}</h6>
-        <p>Doninant Pollutant: {dominentpol}</p>
-        <p>Last Updated: {time.s}</p>
+    <div className={`card mb-4`}>
+        <div className='card-body'>
+        <h5 className='card-title'>{city.name}</h5>
+        <h6 className='card-subtitle mb-2'>Air Quality Index: {aqi}</h6>
+        <p className='card-text'>Doninant Pollutant: {dominentpol}</p>
+        <p className='card-text'>Last Updated: {time.s}</p>
+        </div>
     </div>
   )
 }
