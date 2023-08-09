0e905039613358c494c1e3d4a2c8839422d91e23
Yu <yuhwachn@gmail.com>
2023-08-08 23:14:26 -0400

    got data from api

---
+++ air-quality-app/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import './App.css';
+import CitySearch from './CitySearch';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import AirQualityCard from './AirQualityCard'
+
+function App() {
+  const[airQualityData, setAirQualityData] = useState(null)
+  const [error, setError] = useState(null)
+
+  const getAirQuality = async (city) => {
+    try {
+      //get json from API and store it in "data"
+      const response = await fetch(`https://api.waqi.info/feed/${city}/?token=${process.env.REACT_APP_AQI_API_TOKEN}`)
+      const data = await response.json()
+      //and console log it
+      console.log(data)
+      if(response.ok && data.status === 'ok') {
+        setAirQualityData(data.data)
+        setError(null)
+      } else {
+        setError("Sorry, we couldn't find the city you were looking for. Try another location nearby or ensure your spelling is correct.")
+        setAirQualityData(null)
+      }
+    } catch (error) {
+      console.error("network error:", error)
+      //set error state
+      setError("Sorry, something went wrong.")
+      //set air quality data to null
+      setAirQualityData(null)
+    }
+  }
+  return (
+    <div className='container'>
+      <h1 className='mt-5 mb-3'>Air Quality Index Checker</h1>
+      <CitySearch getAirQuality={getAirQuality}/>
+      {error && (
+        <div className='alert alert-danger' role='alert'>
+          {error}
+        </div>
+      )}
+      {airQualityData && (
+        //Air Quality Card Component
+        //Pollutant  Info
+        <>
+        <AirQualityCard data={airQualityData}/>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
